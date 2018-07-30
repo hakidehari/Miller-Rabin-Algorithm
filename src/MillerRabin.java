91cbe3e21d73f43d8898d5855f6e8cd351b79a60
hakidehari <hakidehari@outlook.com>
2018-07-30 19:12:25 +0200

    prettying up

--- src/MillerRabin.java
+++ src/MillerRabin.java
@@ -10,20 +10,20 @@ import java.util.Scanner;
 public class Part1 {
 
 	public static void main(String[] args) {
-		Part1 p1 = new Part1();
-		
-		// TODO Auto-generated method stub
+			Part1 p1 = new Part1();
+
+			// TODO Auto-generated method stub
 			BigInteger num;
 			Scanner in = new Scanner(System.in);
 			System.out.print("Please input any number to find the closest prime number to it: ");
 			num = in.nextBigInteger();
-			
+
 			System.out.println(p1.getPrime(num));
-			
+
 			in.close();
-			
+
 	}
-	
+
 	public BigInteger getPrime(BigInteger num){
 		BigInteger original,num1;
 		original = num;
@@ -46,11 +46,11 @@ public class Part1 {
 		if (MillerRabin(original) == true){
 			return original;
 		}//end if
-		
+
 		return BigInteger.ZERO;
 	}
-	
-	
+
+
 	//method to test the primality of a number
 	public boolean MillerRabin(BigInteger num){
 		//base cases
@@ -68,17 +68,17 @@ public class Part1 {
 		BigInteger k = BigInteger.valueOf(0);
 		BigInteger q;
 		BigInteger rightSide = nMinusOne;
-		
+
 		//while loop used to find k and q for n-1 = 2^k * q
 		while (rightSide.mod(BigInteger.valueOf(2)).compareTo(BigInteger.valueOf(0)) == 0){
 			rightSide = rightSide.divide(BigInteger.valueOf(2));
 			k = k.add(BigInteger.valueOf(1));
 		}//end while loop
 		q = rightSide;
-		
+
 		//for loop to test primality for 10 different values of a
 		for (int i = 0; i < 10; i++){
-			BigInteger a = randomBigInteger(num);	
+			BigInteger a = randomBigInteger(num);
 			if ((a.modPow(q, num).compareTo(BigInteger.valueOf(1)) == 0)){
 				return true;
 			}//end if
@@ -91,34 +91,34 @@ public class Part1 {
 				}//end nested if
 			}//end nested for loop
 		}//end for loop
-		
+
 		return false;
 	}//end method
-	
+
 	//method to generate a random BigInteger between 1 and n-1
 	public BigInteger randomBigInteger(BigInteger n) {
-        Random rnd = new Random();
-        int maxNumBitLength = n.bitLength();
-        BigInteger aRandomBigInt;
-        do {
+      Random rnd = new Random();
+      int maxNumBitLength = n.bitLength();
+      BigInteger aRandomBigInt;
+      do {
             aRandomBigInt = new BigInteger(maxNumBitLength, rnd);
             // compare random number less than given number
-        } while (aRandomBigInt.compareTo(n) > 0); 
-        return aRandomBigInt;
-    }//end method
-	
+      } while (aRandomBigInt.compareTo(n) > 0);
+      	return aRandomBigInt;
+  }//end method
+
 	//function will generate a random number with a user specified length
-	public String getRandomNumber(int length) 
-	{    
+	public String getRandomNumber(int length)
+	{
 		Random rnd = new Random();
-		
+
 		StringBuilder sb = new StringBuilder(length);
-		
+
 		for (int i = 0; i < length; i++)
 			sb.append((char)('0' + rnd.nextInt(10)));
-        
+
         return sb.toString();
-        
+
     }//end getRandomNumber
-	
+
 }
